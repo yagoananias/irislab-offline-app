270c49578a597c1f166dde2d45ce1f4e845c3119
Yago Ananias <yago_souza92@yahoo.com.br>
2020-05-22 21:48:33 -0300

    Estilizando Home

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{flex: 2,justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Oi</Text>
+        <Text style={{}}>Home</Text>
+        <Image source={require("../assets/asset2.png")} style={{width: 100, height: 100, borderRadius: 100/2, borderWidth: 2, borderColor: '#333'}}  />
       </View>
     )
   }
