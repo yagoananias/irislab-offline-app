adfac1e6d8c2657f377c74ac25275debb057e373
Yago Ananias <yago_souza92@yahoo.com.br>
2020-05-23 15:19:09 -0300

    Editando Home

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -4,44 +4,44 @@ import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 export default class HomeScreen extends React.Component {
   render() {
     return (
-      <View styles={{flex: 1, justifyContent: 'center', paddingHorizontal: 30, paddingVertical: 100}} >
+      <View styles={styles.container} >
         
         <View style={{alignItems: 'center'}}>
-          <Text style={{justifyContent: 'center', paddingVertical: 50, fontSize: 30, fontWeight: 'bold', color: '#9256b1'}}>Home</Text>
+          <Text style={styles.titleTop}>Home</Text>
         </View>
 
-        <TouchableOpacity style={{backgroundColor: '#C68DF8', borderRadius: 100/2, marginBottom: 8, marginHorizontal: 20}}>
-          <View style={{justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 70}}>
-            <Image source={require("../assets/asset2.png")} style={{width: 80, height: 80, borderRadius: 100/2, borderWidth: 1, borderColor: '#9256b1'}}  />
-            <Text style={{alignItems: 'center', fontSize: 20, color: 'white'}}>Tarefa 01</Text>
+        <TouchableOpacity style={styles.touchImpar}>
+          <View style={styles.viewItens}>
+            <Image source={require("../assets/asset2.png")} style={styles.viewImage}  />
+            <Text style={styles.textTarefas}>Tarefa 01</Text>
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={{backgroundColor: '#8ed4d3', borderRadius: 100/2, marginBottom: 8, marginHorizontal: 20}}>
-          <View style={{justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 70}}>
-            <Image source={require("../assets/asset2.png")} style={{width: 80, height: 80, borderRadius: 70/2, borderWidth: 1, borderColor: '#9256b1'}}  />
-            <Text style={{alignItems: 'center', fontSize: 20, color: 'white'}}>Tarefa 02</Text>
+        <TouchableOpacity style={styles.touchPar}>
+          <View style={styles.viewItens}>
+            <Image source={require("../assets/asset2.png")} style={styles.viewImage}  />
+            <Text style={styles.textTarefas}>Tarefa 02</Text>
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={{backgroundColor: '#C68DF8', borderRadius: 100/2, marginBottom: 8, marginHorizontal: 20}}>
-          <View style={{justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 70}}>
-            <Image source={require("../assets/asset2.png")} style={{width: 80, height: 80, borderRadius: 100/2, borderWidth: 1, borderColor: '#9256b1'}}  />
-            <Text style={{alignItems: 'center', fontSize: 20, color: 'white'}}>Tarefa 03</Text>
+        <TouchableOpacity style={styles.touchImpar}>
+          <View style={styles.viewItens}>
+            <Image source={require("../assets/asset2.png")} style={styles.viewImage}  />
+            <Text style={styles.textTarefas}>Tarefa 03</Text>
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={{backgroundColor: '#8ed4d3', borderRadius: 100/2, marginBottom: 8, marginHorizontal: 20}}>
-          <View style={{justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 70}}>
-            <Image source={require("../assets/asset2.png")} style={{width: 80, height: 80, borderRadius: 100/2, borderWidth: 1, borderColor: '#9256b1'}}  />
-            <Text style={{alignItems: 'center', fontSize: 20, color: 'white'}}>Tarefa 04</Text>
+        <TouchableOpacity style={styles.touchPar}>
+          <View style={styles.viewItens}>
+            <Image source={require("../assets/asset2.png")} style={styles.viewImage}  />
+            <Text style={styles.textTarefas}>Tarefa 04</Text>
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={{backgroundColor: '#C68DF8', borderRadius: 100/2, marginBottom: 8, marginHorizontal: 20}}>
-          <View style={{justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 70}}>
-            <Image source={require("../assets/asset2.png")} style={{width: 80, height: 80, borderRadius: 100/2, borderWidth: 1, borderColor: '#9256b1'}}  />
-            <Text style={{alignItems: 'center', fontSize: 20, color: 'white'}}>Tarefa 05</Text>
+        <TouchableOpacity style={styles.touchImpar}>
+          <View style={styles.viewItens}>
+            <Image source={require("../assets/asset2.png")} style={styles.viewImage}  />
+            <Text style={styles.textTarefas}>Tarefa 05</Text>
           </View>
         </TouchableOpacity>
 
@@ -49,4 +49,53 @@ export default class HomeScreen extends React.Component {
     )
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 100
+  },
+  titleTop: {
+    justifyContent: 'center',
+    paddingVertical: 50,
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#9256b1'
+  },
+  touchImpar: {
+    backgroundColor: '#C68DF8',
+    borderRadius: 100/2,
+    marginBottom: 8,
+    marginHorizontal: 20
+  },
+  touchPar: {
+    backgroundColor: '#8ed4d3',
+    borderRadius: 100/2,
+    marginBottom: 8,
+    marginHorizontal: 20
+  },
+  viewItens: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 70
+  },
+  viewImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 100/2,
+    borderWidth: 1,
+    borderColor: '#9256b1'
+  },
+  textTarefas: {
+    alignItems: 'center',
+    fontSize: 20,
+    color: 'white'
+  }
+
+})
+
+
 
